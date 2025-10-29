a5773a1902ef99e94064b410e83ecde1e0f8e1b7
plan11plan <tjtfy7028@gmail.com>
2025-10-30 02:12:25 +0900

    feat(InputView): 보너스 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.model.BonusNumber;
 import lotto.model.PurchaseAmount;
 import lotto.model.WinningNumbers;
 import lotto.view.InputView;
@@ -9,9 +10,12 @@ public class Application {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
         PurchaseAmount purchaseAmount = inputView.readPurchaseAmount();
-        System.out.println(purchaseAmount.money());
+        System.out.println(purchaseAmount.toString());
 
         WinningNumbers winningNumbers = inputView.readWinningNumbers();
         System.out.println(winningNumbers.numbers().toString());
+
+        BonusNumber bonusNumber = inputView.readBonusNumber(winningNumbers);
+        System.out.println(bonusNumber.number());
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import lotto.model.BonusNumber;
 import lotto.model.PurchaseAmount;
 import lotto.model.WinningNumbers;
 import lotto.util.StringParser;
@@ -9,9 +10,9 @@ import lotto.util.StringParser;
 public class InputView {
 
     public PurchaseAmount readPurchaseAmount() {
-        System.out.println("구매금액을 입력해 주세요.");
         while (true) {
             try {
+                System.out.println("구매금액을 입력해 주세요.");
                 String input = Console.readLine();
                 if (input.isEmpty()) {
                     throw new IllegalArgumentException("[ERROR] 구매금액이 비어 있습니다.");
@@ -27,9 +28,9 @@ public class InputView {
     }
 
     public WinningNumbers readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
         while (true) {
             try {
+                System.out.println("당첨 번호를 입력해 주세요.");
                 String input = Console.readLine();
                 if (input.isEmpty()) {
                     throw new IllegalArgumentException("[ERROR] 당첨 번호가 비어 있습니다.");
@@ -42,6 +43,23 @@ public class InputView {
                 System.out.print(System.lineSeparator());
             }
         }
+    }
 
+    public BonusNumber readBonusNumber(WinningNumbers winningNumbers) {
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                if (input.isEmpty()) {
+                    throw new IllegalArgumentException("[ERROR] 보너스 번호가 비어 있습니다.");
+                }
+                int bonusNumber = StringParser.parseInt(input);
+                return BonusNumber.from(bonusNumber, winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.print(System.lineSeparator());
+            }
+        }
     }
 }
