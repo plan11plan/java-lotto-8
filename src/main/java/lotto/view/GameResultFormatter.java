b4e2771457ac58725a6420458a48d1b154a17325
plan11plan <tjtfy7028@gmail.com>
2025-10-31 01:31:37 +0900

    refactor(GameResultFormatter): 포맷팅 문자 분리

--- src/main/java/lotto/view/GameResultFormatter.java
+++ src/main/java/lotto/view/GameResultFormatter.java
@@ -1,38 +1,52 @@
 package lotto.view;
 
+import static java.lang.String.format;
+import static java.lang.System.lineSeparator;
+import static lotto.model.LotteryRank.FIFTH;
+import static lotto.model.LotteryRank.FIRST;
+import static lotto.model.LotteryRank.FOURTH;
+import static lotto.model.LotteryRank.SECOND;
+import static lotto.model.LotteryRank.THIRD;
+
 import java.util.List;
 import java.util.Map;
 import lotto.model.LotteryRank;
 import lotto.model.Lotto;
 
 public class GameResultFormatter {
+    private static final String PURCHASE_RESULT = "%d개를 구매했습니다.";
+    private static final String WINNING_RESULT_TITLE = "당첨 통계";
+    private static final String RESULT_LINE = "---";
+    private static final String RANK_RESULT_FORMAT = "%d개 일치 (%,d원) - %d개";
+    private static final String RANK_RESULT_WITH_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
+
 
     public String formatLottoPurchaseResults(int lottoCount, List<Lotto> lottos) {
         StringBuilder sb = new StringBuilder();
-        sb.append(lottoCount).append("개를 구매했습니다.").append(System.lineSeparator());
+        sb.append(format(PURCHASE_RESULT, lottoCount)).append(lineSeparator());
         for (Lotto lotto : lottos) {
-            sb.append(lotto.numbers()).append(System.lineSeparator());
+            sb.append(lotto.numbers()).append(lineSeparator());
         }
-        sb.append(System.lineSeparator());
+        sb.append(lineSeparator());
         return sb.toString();
     }
 
 
     public String formatWinningStatistics(Map<LotteryRank, Integer> rankCounts, double profitRate) {
         StringBuilder sb = new StringBuilder();
-        sb.append("당첨 통계").append(System.lineSeparator());
-        sb.append("---").append(System.lineSeparator());
-        sb.append("3개 일치 (").append(String.format("%,d", LotteryRank.FIFTH.getPrizeMoney())).append("원) - ")
-                .append(rankCounts.get(LotteryRank.FIFTH)).append("개").append(System.lineSeparator());
-        sb.append("4개 일치 (").append(String.format("%,d", LotteryRank.FOURTH.getPrizeMoney())).append("원) - ")
-                .append(rankCounts.get(LotteryRank.FOURTH)).append("개").append(System.lineSeparator());
-        sb.append("5개 일치 (").append(String.format("%,d", LotteryRank.THIRD.getPrizeMoney())).append("원) - ")
-                .append(rankCounts.get(LotteryRank.THIRD)).append("개").append(System.lineSeparator());
-        sb.append("5개 일치, 보너스 볼 일치 (").append(String.format("%,d", LotteryRank.SECOND.getPrizeMoney())).append("원) - ")
-                .append(rankCounts.get(LotteryRank.SECOND)).append("개").append(System.lineSeparator());
-        sb.append("6개 일치 (").append(String.format("%,d", LotteryRank.FIRST.getPrizeMoney())).append("원) - ")
-                .append(rankCounts.get(LotteryRank.FIRST)).append("개").append(System.lineSeparator());
-        sb.append("총 수익률은 ").append(profitRate).append("%입니다.");
+        sb.append(WINNING_RESULT_TITLE).append(lineSeparator());
+        sb.append(RESULT_LINE).append(lineSeparator());
+
+        sb.append(format(RANK_RESULT_FORMAT, 3, FIFTH.getPrizeMoney(), rankCounts.get(FIFTH))).append(lineSeparator());
+        sb.append(format(RANK_RESULT_FORMAT, 4, FOURTH.getPrizeMoney(), rankCounts.get(FOURTH)))
+                .append(lineSeparator());
+        sb.append(format(RANK_RESULT_FORMAT, 5, THIRD.getPrizeMoney(), rankCounts.get(THIRD))).append(lineSeparator());
+        sb.append(format(RANK_RESULT_WITH_BONUS_FORMAT, 5, SECOND.getPrizeMoney(), rankCounts.get(SECOND)))
+                .append(lineSeparator());
+        sb.append(format(RANK_RESULT_FORMAT, 6, FIRST.getPrizeMoney(), rankCounts.get(FIRST))).append(lineSeparator());
+
+        sb.append(String.format(PROFIT_RATE_FORMAT, profitRate));
         return sb.toString();
     }
 }
